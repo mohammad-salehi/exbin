8c8c83a9cd87f5335de77aa943f9d5e60911bba1
mohammad <mohammad7979salehi@gmail.com>
2025-09-07 17:00:03 +0330

    login api

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -5,11 +5,31 @@ type LoaderProps = {
 
 export const LoaderCircle = ({ size, color }: LoaderProps) => {
     return (
-        <div aria-label="Loading" role="alert" aria-busy="true" className={`w-${size} h-${size} relative rounded-full rtl:-scale-x-100`}>
-            <div className={`border-4 ${color} absolute w-full h-full rounded-full animate-spin border-x-transparent border-b-transparent`} style={{ animationDelay: "-0.45s" }} role="presentation"></div>
-            <div className={`border-4 ${color} absolute w-full h-full rounded-full animate-spin border-x-transparent border-b-transparent`} style={{ animationDelay: "-0.3s" }} role="presentation"></div>
-            <div className={`border-4 ${color} absolute w-full h-full rounded-full animate-spin border-x-transparent border-b-transparent`} style={{ animationDelay: "-0.15s" }} role="presentation"></div>
-            <div className={`border-4 ${color} absolute w-full h-full rounded-full animate-spin border-x-transparent border-b-transparent`} role="presentation"></div>
+        <div
+            aria-label="Loading"
+            role="alert"
+            aria-busy="true"
+            className={`w-${size} h-${size} relative flex justify-center items-center rounded-full rtl:-scale-x-100`}
+        >
+            <div
+                className={`border-4 ${color} absolute w-8 h-full rounded-full animate-spin border-x-transparent border-b-transparent`}
+                style={{ animationDelay: "-0.45s" }}
+                role="presentation"
+            ></div>
+            <div
+                className={`border-4 ${color} absolute w-8 h-full rounded-full animate-spin border-x-transparent border-b-transparent`}
+                style={{ animationDelay: "-0.3s" }}
+                role="presentation"
+            ></div>
+            <div
+                className={`border-4 ${color} absolute w-8 h-full rounded-full animate-spin border-x-transparent border-b-transparent`}
+                style={{ animationDelay: "-0.15s" }}
+                role="presentation"
+            ></div>
+            <div
+                className={`border-4 ${color} absolute w-8 h-full rounded-full animate-spin border-x-transparent border-b-transparent`}
+                role="presentation"
+            ></div>
         </div>
-    )
-}
+    );
+};
