3d52bf7ace6a461ef64fcdc9725d4dcf50e2c090
mohammad <mohammad7979salehi@gmail.com>
2025-09-07 16:25:40 +0330

    login api

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,12 +1,8 @@
-// src/types/auth.ts
-
-/** درخواست لاگین */
 export interface LoginRequest {
     username: string;
     password: string;
 }
 
-/** ریسپانس موفق */
 export interface LoginResponse {
     result: { token: string };
     error: null;
