d0be784f70dd5ac3d7267a79343985b585c02115
mohammad <mohammad7979salehi@gmail.com>
2025-09-16 15:00:28 +0330

    mob

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,6 @@ export default function RootLayout({
     <html lang="fa" dir="rtl">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {/* سایر تگ‌های متا */}
       </Head>
       <body className="bg-bgColor dark:bg-bgColor-dark">
         <ThemeProvider theme={moonDesignLight}>
