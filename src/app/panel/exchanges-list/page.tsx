2672eabb524a4ba0fde7bdafcc0e32a683f726c2
mohammad <mohammad7979salehi@gmail.com>
2025-10-01 13:46:30 +0330

    validations done

--- src/app/panel/exchanges-list/page.tsx
+++ src/app/panel/exchanges-list/page.tsx
@@ -196,7 +196,7 @@ const Page = () => {
       <div className="mb-4">
         {
           Loading && (
-            <div className="fixed inset-0 z-50 grid place-items-center bg-white/70 backdrop-blur-sm">
+            <div className="fixed inset-0 z-50 grid place-items-center bg-white/70 dark:bg-bgColor-dark/70 backdrop-blur-sm">
               <div className="pointer-events-none">
                 <AnimatedText />
               </div>
